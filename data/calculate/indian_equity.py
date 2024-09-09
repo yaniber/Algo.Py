eda9337e5003407add68e90d5f2d00f4c43f46a6
himanshu2406 <himanshuclash@gmail.com>
2024-09-10 02:24:08 +0530

    fix compatibility issues

--- data/calculate/indian_equity.py
+++ data/calculate/indian_equity.py
@@ -9,13 +9,13 @@ def calculate_technical_indicators(market_name, timeframe='1d'):
     '''
     fetch_calculate_and_insert(market_name=market_name, timeframe=timeframe, calculation_func=calculate_ema, length=100, frequently_accessed=True)
     fetch_calculate_and_insert(market_name=market_name, timeframe=timeframe, calculation_func=calculate_ema, length=200, frequently_accessed=True)
-    fetch_calculate_and_insert(market_name=market_name, timeframe=timeframe, calculation_func=calculate_supertrend, atr_multiplier=3.0, length=10, frequently_accessed=False)
-    fetch_calculate_and_insert(market_name=market_name, timeframe=timeframe, calculation_func=calculate_exponential_regression, window=90, frequently_accessed=False)
-    fetch_calculate_and_insert(market_name=market_name, timeframe=timeframe, calculation_func=calculate_exponential_regression, window=30, frequently_accessed=False)
-    fetch_calculate_and_insert(market_name=market_name, timeframe=timeframe, calculation_func=calculate_exponential_regression, window=15, frequently_accessed=False)
-    fetch_calculate_and_insert(market_name=market_name, timeframe=timeframe, calculation_func=calculate_spike, lookback_period=90, spike_threshold=0.15, frequently_accessed=False)
-    fetch_calculate_and_insert(market_name=market_name, timeframe=timeframe, calculation_func=detect_large_gap, lookback_period=90, gap_threshold=0.15, frequently_accessed=False)
-    fetch_calculate_and_insert(market_name=market_name, timeframe=timeframe, calculation_func=calculate_average_volume, lookback_period=90, frequently_accessed=False)
+    fetch_calculate_and_insert(market_name=market_name, timeframe=timeframe, calculation_func=calculate_supertrend, atr_multiplier=3.0, length=10, frequently_accessed=True)
+    fetch_calculate_and_insert(market_name=market_name, timeframe=timeframe, calculation_func=calculate_exponential_regression, window=90, frequently_accessed=True)
+    fetch_calculate_and_insert(market_name=market_name, timeframe=timeframe, calculation_func=calculate_exponential_regression, window=30, frequently_accessed=True)
+    fetch_calculate_and_insert(market_name=market_name, timeframe=timeframe, calculation_func=calculate_exponential_regression, window=15, frequently_accessed=True)
+    fetch_calculate_and_insert(market_name=market_name, timeframe=timeframe, calculation_func=calculate_spike, lookback_period=90, spike_threshold=0.15, frequently_accessed=True)
+    fetch_calculate_and_insert(market_name=market_name, timeframe=timeframe, calculation_func=detect_large_gap, lookback_period=90, gap_threshold=0.15, frequently_accessed=True)
+    fetch_calculate_and_insert(market_name=market_name, timeframe=timeframe, calculation_func=calculate_average_volume, lookback_period=90, frequently_accessed=True)
 
 if __name__ == "__main__":
     calculate_technical_indicators(market_name='indian_equity', timeframe='1d')
